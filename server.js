933b55896abd0fe0b7e595bf40080f40abc9d15a
faeemxccc <naeemansari962n@gmail.com>
2025-09-06 12:34:16 +0530

    Prepare for Vercel deployment

--- server.js
+++ server.js
@@ -27,5 +27,5 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
